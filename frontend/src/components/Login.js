9c4e7327d23a4824a0fcb3211f3398c603d97e78
Hopeson281223 <benderihopeson281223@gmail.com>
2025-03-22 08:40:09 +0300

    render link

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -30,7 +30,7 @@ const Login = () => {
 
         setLoading(true);
         try {
-            const res = await fetch("http://localhost:5000/api/login", {
+            const res = await fetch("https://classattendanceqrcodesystem.onrender.com/api/login", {  // Updated to Render URL
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(user),
