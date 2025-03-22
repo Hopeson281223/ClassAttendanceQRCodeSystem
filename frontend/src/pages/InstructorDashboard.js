da8f8a4e21251f49c93a8199299b07da5df86330
Hopeson281223 <benderihopeson281223@gmail.com>
2025-03-22 09:44:29 +0300

    Update InstructorDashboard.js

--- frontend/src/pages/InstructorDashboard.js
+++ frontend/src/pages/InstructorDashboard.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { QRCodeCanvas } from "qrcode.react";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://classattendanceqrcodesystem.onrender.com";
 
 const InstructorDashboard = () => {
     const [sessionName, setSessionName] = useState("");
